f2258df0bd30ceb9c60d7e6c78d16843e7cf1c62
julien-muke <julien.muke@gmail.com>
2024-03-08 15:00:02 -0800

    🚀 install react-just-parallax

--- src/components/design/Header.jsx
+++ src/components/design/Header.jsx
@@ -28,7 +28,7 @@ export const BackgroundCircles = () => {
   );
 };
 
-export const HamburgerMenu = () => {
+export const HambugerMenu = () => {
   return (
     <div className="absolute inset-0 pointer-events-none lg:hidden">
       <div className="absolute inset-0 opacity-[.03]">
